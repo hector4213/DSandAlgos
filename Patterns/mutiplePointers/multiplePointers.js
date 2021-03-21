eade8a2e6f8c479b0194abb821c6f4b0906cb1b3
hector4213 <hectorpwns@gmail.com>
2021-03-21 08:37:49 -0400

    fix comments

---
+++ Patterns/mutiplePointers/multiplePointers.js
@@ -0,0 +1,4 @@
+/**
+ * MUTIPLE POINTERS: Creating pointers or values that correspond to an index or position and move towards the beginning, end, or middle
+ * based on a certain condition. Very efficient for solving problems with minimal space complexity as well.
+ */
