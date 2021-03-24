5694d2d675b639410967b425637660f4186628f6
hector4213 <hectorpwns@gmail.com>
2021-03-24 08:04:43 -0400

    add challenge ex

---
+++ Patterns/mutiplePointers/mutiplePointersChallenge.js
@@ -0,0 +1,4 @@
+/**
+ * Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array.
+ * There can be negative numbers in the array, but it will always be sorted.
+ */
