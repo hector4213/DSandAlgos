9d8526c3d7191e7d048afe54d74f6290d5d5c758
hector4213 <hectorpwns@gmail.com>
2021-03-29 22:45:50 -0400

    start sliding window

---
+++ Patterns/slidingWindowPattern/slidingWindow.js
@@ -0,0 +1,5 @@
+/**
+ * This pattern involves creating a window which  can either be an array or number from one position to another.
+ * Depending on a certain condition, the window either increases or closes.( and a new window is created). Useful
+ * for keeping track of a subset of data in an array / string.
+ */
