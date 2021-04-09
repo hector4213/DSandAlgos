c63459c5f4e73acd250b718f832a6ecfa3db02a7
hector4213 <hectorpwns@gmail.com>
2021-04-08 23:35:52 -0400

    add probnlem

--- Recursion/recursionProblemSet.js
+++ Recursion/recursionProblemSet.js
@@ -11,3 +11,16 @@ const power = (base, exponent) => {
 console.log(power(2, 4) === 16 ? 'PASS' : 'FAIL')
 console.log(power(2, 0) === 1 ? 'PASS' : 'FAIL')
 console.log(power(2, 2) === 4 ? 'PASS' : 'FAIL')
+
+/**
+ * Write a function called factorial which accepts a number and return the factorial of that number. A factorial is the products of an integer
+ * and all the integers below it, e.g, factorial( 4!) is equal to 24 because, 4 * 3 * 2 * 1 === 24. Factorial zero (0!) is always 1.
+ *
+ */
+
+const factorial = (num) => {}
+
+console.log(factorial(1) === 1 ? 'PASS' : 'FAIL')
+console.log(factorial(2) === 2 ? 'PASS' : 'FAIL')
+console.log(factorial(4) === 24 ? 'PASS' : 'FAIL')
+console.log(factorial(7) === 5040 ? 'PASS' : 'FAIL')
