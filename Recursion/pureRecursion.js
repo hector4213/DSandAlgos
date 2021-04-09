8c5be7c486ad4a2718f8ade5da0544bc7cf4f474
hector4213 <hectorpwns@gmail.com>
2021-04-08 23:19:15 -0400

    pure recurion example

---
+++ Recursion/pureRecursion.js
@@ -0,0 +1,14 @@
+/**
+ * Pure Recursion Example
+ */
+
+const collectOddValues = (arr) => {
+	let newArr = []
+
+	if (arr.length === 0) return newArr
+	if (arr[0] % 2 !== 0) {
+		newArr.push(arr[0])
+	}
+	newArr.concat(collectOddValues(arr.slice(1)))
+	return newArr
+}
